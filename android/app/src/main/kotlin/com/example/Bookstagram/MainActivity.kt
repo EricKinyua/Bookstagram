71f8e7460d8b20fe36472331e82aea3dcc4ec533
Eric Kinyua <eriquekinyua0@gmail.com>
2020-07-02 20:45:05 +0300

    Books

---
+++ android/app/src/main/kotlin/com/example/Bookstagram/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Bookstagram
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/homepage.dart
@@ -0,0 +1,282 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFEDF0F6),
+      body: ListView(
+        physics: AlwaysScrollableScrollPhysics(),
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Bookstagram',
+                  style: TextStyle(
+                    fontFamily: 'Billabong',
+                    fontSize: 32,
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.live_tv),
+                      iconSize: 30,
+                      onPressed: () => print('BTV'),
+                    ),
+                    SizedBox(
+                      width: 16,
+                    ),
+                    Container(
+                      width: 35,
+                      child: IconButton(
+                        icon: Icon(Icons.send),
+                        iconSize: 30,
+                        onPressed: () => print('Direct Messages'),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 100,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: null,
+              itemBuilder: (BuildContext context, int index) {
+                if (index == 0) {
+                  return SizedBox(
+                    width: 10,
+                  );
+                }
+                return Container(
+                  margin: EdgeInsets.all(10),
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(shape: BoxShape.circle, boxShadow: [
+                    BoxShadow(
+                      color: Colors.black45,
+                      offset: Offset(0, 2),
+                      blurRadius: 6,
+                    )
+                  ]),
+                  child: CircleAvatar(
+                    child: ClipOval(
+                      child: Image(
+                        height: 60,
+                        width: 60,
+                        image: NetworkImage(
+                            'https://images.unsplash.com/photo-1521676259650-675b5bfec1ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: Container(
+              width: double.infinity,
+              height: 560,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    child: Column(
+                      children: <Widget>[
+                        ListTile(
+                          leading: Container(
+                            width: 50,
+                            height: 50,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black45,
+                                    offset: Offset(0, 2),
+                                    blurRadius: 6,
+                                  )
+                                ]),
+                            child: CircleAvatar(
+                              child: ClipOval(
+                                child: Image(
+                                  height: 50,
+                                  width: 50,
+                                  image: NetworkImage(
+                                      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80'),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ),
+                          title: Text(
+                            'Eric Kinyua',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          trailing: IconButton(
+                            icon: Icon(Icons.more_horiz),
+                            color: Colors.black,
+                            onPressed: () => print('More'),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.all(10),
+                          width: double.infinity,
+                          height: 400,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black45,
+                                offset: Offset(0, 5),
+                                blurRadius: 8,
+                              ),
+                            ],
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                  'https://images.unsplash.com/photo-1534523600533-71d091d911d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80'),
+                              fit: BoxFit.fitWidth,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Row(
+                                children: <Widget>[
+                                                              Row(
+                                children: <Widget>[
+                                  IconButton(
+                                    icon: Icon(Icons.favorite_border),
+                                    iconSize: 30,
+                                    onPressed: () => print('Like Post'),
+                                  ),
+                                  Text(
+                                    '2,515,',
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  )
+                                ],
+                              ),
+                              SizedBox(
+                                  width: 20,
+                              ),
+                              Row(
+                                children: <Widget>[
+                                  IconButton(
+                                    icon: Icon(Icons.chat),
+                                    iconSize: 30,
+                                    onPressed: () => print('Comment'),
+                                  ),
+                                  Text(
+                                    '350',
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                                ],
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.bookmark_border), 
+                                iconSize: 30,
+                                onPressed: () => print('Save Post'),
+                                ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+              child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: [
+           BottomNavigationBarItem(
+             icon: Icon(
+               Icons.dashboard,
+               size: 30,
+               color: Colors.black,
+             ),
+             title: Text('data'),
+             ),
+             BottomNavigationBarItem(
+             icon: Icon(
+               Icons.search,
+               size: 30,
+               color: Colors.grey,
+             ),
+             title: Text('data'),
+             ),
+              BottomNavigationBarItem(
+             icon: Padding(
+               padding: const EdgeInsets.symmetric(horizontal: 5,
+               vertical: 10
+               ),
+               child: FlatButton(
+                 padding: EdgeInsets.symmetric(vertical: 10),
+                 shape: RoundedRectangleBorder(
+                   borderRadius: BorderRadius.circular(30),
+
+                 ),
+                 color: Color(0xFF23B66F),
+                 onPressed: () => print('Upload photo'),
+                 child: Icon(
+                   Icons.add,
+                   size: 35,
+                   color: Colors.white,
+                 ),
+                 ),
+             ),
+             title: Text('data'),
+             ),
+             BottomNavigationBarItem(
+             icon: Icon(
+               Icons.favorite_border,
+               size: 30,
+               color: Colors.grey,
+             ),
+             title: Text('data'),
+             ),
+             BottomNavigationBarItem(
+             icon: Icon(
+               Icons.person_outline,
+               size: 30,
+               color: Colors.grey,
+             ),
+             title: Text('data'),
+             ),
+          ]),
+      ),
+    );
+  }
+}
